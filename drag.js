83b7e178448ac4b17b15f8bb6798529874b910bc
13145997119 <1554720440@qq.com>
2020-06-24 09:32:56 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -2,7 +2,8 @@ function drag(id){
   var obj=document.getElementById(id)
   var disX=0;
   var disY=0;
-  obj.onmousedown=function(){
-      
-  }
+  obj.onmousedown=function(ev){
+       disX=ev.pageX-obj.offsetLeft;
+       disY=ev.pageY-obj.offsetTop
+  } 
 }
